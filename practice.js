e05e10a16f2cf9b8f4949cf8745f93a3c35f009a
moe <moe.faridd@gmail.com>
2020-11-18 00:44:54 -0500

    array practice

---
+++ practice.js
@@ -0,0 +1,18 @@
+myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+console.log(myArray.join())
+//1,2,3,4,5,6,7,8,9,10
+console.log(myArray.join(" "))
+//1 2 3 4 5 6 7 8 9 10
+console.log(myArray.join('-'))
+//1-2-3-4-5-6-7-8-9-10
+
+myStr = "Hello World"
+console.log(myStr.split())
+//[ 'Hello World' ]
+console.log(myStr.split(" "))
+// /[ 'Hello', 'World' ]
+console.log(myStr.split(""))
+// [  'H', 'e', 'l', 'l',
+    // 'o', ' ', 'W', 'o',
+    // 'r', 'l', 'd']
